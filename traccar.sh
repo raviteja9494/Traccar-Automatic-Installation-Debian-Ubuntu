e12cda237a947ee3df5051b9eac63e4ff406ccb6
raviteja9494 <raviteja.vadapalli94@gmail.com>
2025-04-27 14:32:46 +0530

    Update traccar.sh

--- traccar.sh
+++ traccar.sh
@@ -1,59 +1,64 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade -y
-apt-get install wget
-apt install net-tools
-apt-get install unzip -y
-apt-get install unzip default-jre mysql-server -y
-/ Not required in actual script
-MYSQL_ROOT_PASSWORD=!Traccar321
-SECURE_MYSQL=$(expect -c "
-set timeout 10
-spawn mysql_secure_installation
-expect \"Enter current password for root (enter for none):\"
-send \"$MYSQL\r\"
-expect \"Change the root password?\"
-send \"n\r\"
-expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"y\r\"
-expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
-expect eof
-")
 
-echo "$SECURE_MYSQL"
- mysql -u root -p!Traccar321 -e "create database new_d"
-  mysql -u root -p!Traccar321 -e "CREATE USER 'traccar1'@'localhost' IDENTIFIED BY '!Traccar321'"
-  mysql -u root -p!Traccar321 -e "GRANT ALL PRIVILEGES ON new_d.* TO traccar1@localhost"
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Define MySQL root password and Traccar database credentials
+MYSQL_ROOT_PASSWORD='!Traccar321'
+TRACCAR_DB='traccar_db'
+TRACCAR_USER='traccar1'
+TRACCAR_PASSWORD='!Traccar321'
+
+# Update and upgrade system packages
+apt-get update && apt-get upgrade -y
+
+# Install necessary packages
+apt-get install -y wget unzip default-jre mysql-server net-tools ufw
+
+# Secure MySQL installation
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
+
+# Create Traccar database and user
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE ${TRACCAR_DB} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER '${TRACCAR_USER}'@'localhost' IDENTIFIED BY '${TRACCAR_PASSWORD}';"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${TRACCAR_DB}.* TO '${TRACCAR_USER}'@'localhost'; FLUSH PRIVILEGES;"
+
+# Download and install Traccar
 wget https://www.traccar.org/download/traccar-linux-64-latest.zip
-unzip traccar-linux-*.zip && ./traccar.run
+unzip traccar-linux-64-latest.zip
+chmod +x traccar.run
+./traccar.run
+
+# Configure Traccar to use MySQL and enable specific protocols
 cat > /opt/traccar/conf/traccar.xml << EOF
 <?xml version='1.0' encoding='UTF-8'?>
 
 <!DOCTYPE properties SYSTEM 'http://java.sun.com/dtd/properties.dtd'>
 
 <properties>
-
-    <entry key="config.default">./conf/default.xml</entry>
-	<entry key='web.port'>80</entry>
-
-    <entry key='database.driver'>com.mysql.jdbc.Driver</entry>
-    <entry key='database.url'>jdbc:mysql://localhost/new_d?allowPublicKeyRetrieval=true&amp;useSSL=false&amp;serverTimezone=UTC&amp;useSSL=false&amp;allowMultiQueries=true&amp;autoReconnect=true&amp;useUnicode=yes&amp;characterEncoding=UTF-8&amp;sessionVariables=sql_mode=''</entry>
-    <entry key='database.user'>traccar1</entry>
-    <entry key='database.password'>!Traccar321</entry>
-
+    <entry key='config.default'>./conf/default.xml</entry>
+    <entry key='web.port'>8082</entry>
+    <entry key='database.driver'>com.mysql.cj.jdbc.Driver</entry>
+    <entry key='database.url'>jdbc:mysql://localhost:3306/${TRACCAR_DB}?serverTimezone=UTC&amp;allowPublicKeyRetrieval=true&amp;useSSL=false&amp;allowMultiQueries=true&amp;autoReconnect=true&amp;useUnicode=yes&amp;characterEncoding=UTF-8&amp;sessionVariables=sql_mode=''</entry>
+    <entry key='database.user'>${TRACCAR_USER}</entry>
+    <entry key='database.password'>${TRACCAR_PASSWORD}</entry>
+    <entry key='protocols.enable'>osmand,gt06</entry>
 </properties>
 EOF
-service traccar start
+
+# Start Traccar service
+systemctl start traccar
+systemctl enable traccar
+
+# Configure UFW firewall
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow 22/tcp    # SSH
+ufw allow 8082/tcp  # Traccar Web Interface
+ufw allow 5023/tcp  # GT06 Protocol Port
+ufw --force enable
+
+# Display access information
 clear
-echo "You can login via following IP address to your Traccar platform with following login info:"
-ifconfig eth0 | grep "inet " | awk '{gsub("addr:","",$2);  print $2 }'
-echo "Email: admin"
-echo "Password: admin"
-echo "+-++-++-++-++-++-++-+ +-++-+   +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
-|P||o||w||e||r||e||d| |b||y|   https://github.com/macan0    Need a custom Traccar design: https://www.fiverr.com/users/macano84
-+-++-++-++-++-++-++-+ +-++-+   +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+"
+echo "Traccar installation completed successfully!"
+echo "Access the Traccar web interface at: http://$(hostname -I | awk '{print $1}'):8082"
